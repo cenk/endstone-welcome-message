c97db920e95f9b6a9b4f12d747f948e9abad46d6
cenk <git@cenk.app>
2025-07-21 19:56:25 +0300

    Make public

---
+++ README.md
@@ -0,0 +1,43 @@
+## endstone-welcome-message 
+
+A simple Endstone plugin that allows you to send players a welcome message via chat, tip, toast, title, popup or form when they join the server.
+
+### Message types
+#### 1 - Chat message:
+
+#### 2 - Tip message:
+
+#### 3 - Popup message:
+
+#### 4 - Toast message:
+
+#### 5 - Title message:
+
+#### 6 - Form message:
+
+
+### Placeholders
+You can use the following placeholders in your welcome message. 
+| Placeholder | Description |
+| --- | --- |
+| {player_name} | Players's name |
+| {player_locale} | Player's current locale |
+| {player_device_os} | Player's operation system |
+| {player_device_id} | Player's current device id |
+| {player_hostname} | Player's hostname |
+| {player_port} | Player's port number |
+| {player_game_mode} | Player's current game mode |
+| {player_game_version} | Player's current game version |
+| {player_exp_level} | Playerss current experience level |
+| {player_total_exp} | Player's total experience points |
+| {player_exp_progress} | Player's current experience progress towards the next level |
+| {player_ping} | Player's average ping |
+| {server_level_name} | Server's level name |
+| {server_max_players} | The maximum amount of player's which can login to this server |
+| {server_online_players} | Current online players count |
+| {server_start_time} | Start time of the server |
+| {server_locale} | Server's current locale |
+| {server_endstone_version} | Server's Endstone version |
+| {server_minecraft_version} | Server's Minecraft version |
+| {server_port} | Server's IPv4 port |
+| {server_port_v6} | Server's IPv6 port |
---
+++ src/endstone_welcome_message/welcome_message.py
@@ -0,0 +1,74 @@
+from endstone.plugin import Plugin
+from endstone.event import event_handler, PlayerJoinEvent
+from endstone.form import ModalForm, Label
+import time
+
+class WelcomeMessage(Plugin):
+    api_version = "0.6"
+
+    def on_enable(self) -> None:
+        self.save_default_config()
+        self.register_events(self)
+        self.welcome_message_type = max(0, min(int(self.config["welcome_message"]["type"]), 6))
+
+        if self.welcome_message_type > 0:
+            self.welcome_message_header = str(self.config["welcome_message"]["header"])
+            self.welcome_message_body = str(self.config["welcome_message"]["body"])
+            self.welcome_message_form_button_text = str(self.config["welcome_message"]["form_button_text"])
+            self.welcome_message_wait_before = max(0, min(int(self.config["welcome_message"]["wait_before"]), 5))
+        else:
+            self.logger.info("Welcome Message is disabled in the config file!")
+
+    @event_handler
+    def on_player_join(self, event: PlayerJoinEvent):
+        if self.welcome_message_type > 0:
+            self.replace_placeholders(event.player)
+            if self.welcome_message_wait_before > 0:
+                time.sleep(self.welcome_message_wait_before)
+            match self.welcome_message_type:
+                case 1:
+                    event.player.send_message(self.welcome_message_body)
+                case 2:
+                    event.player.send_tip(self.welcome_message_body)
+                case 3:
+                    event.player.send_popup(self.welcome_message_body)
+                case 4:
+                    event.player.send_toast(self.welcome_message_header, self.welcome_message_body)
+                case 5:
+                    event.player.send_title(self.welcome_message_header, self.welcome_message_body)
+                case 6:
+                    welcome_form = ModalForm(
+                        title = self.welcome_message_header,
+                        controls = [Label(text=self.welcome_message_body + '\n\n')],
+                        submit_button = self.welcome_message_form_button_text
+                    )
+                    event.player.send_form(welcome_form)
+
+    def replace_placeholders(self, player) -> None:
+        placeholder = {
+            'player_name': player.name,
+            'player_locale': player.locale,
+            'player_device_os': player.device_os,
+            'player_device_id': player.device_id,
+            'player_hostname': player.address.hostname,
+            'player_port': player.address.port,
+            'player_game_mode': player.game_mode.name,
+            'player_game_version': player.game_version,
+            'player_exp_level': player.exp_level,
+            'player_total_exp': player.total_exp,
+            'player_exp_progress': player.exp_progress,
+            'player_ping': player.ping,
+            'server_level_name': self.server.level.name,
+            'server_max_players': self.server.max_players,
+            'server_online_players': len(self.server.online_players),
+            'server_start_time': self.server.start_time,
+            'server_locale': self.server.language.locale,
+            'server_endstone_version': self.server.version,
+            'server_minecraft_version': self.server.minecraft_version,
+            'server_port': self.server.port,
+            'server_port_v6': self.server.port_v6
+            # requires 'latest' api
+            #'server_protocol_version': self.server.protocol_version
+        }
+        self.welcome_message_header = self.welcome_message_header.format(**placeholder)
+        self.welcome_message_body = self.welcome_message_body.format(**placeholder)
