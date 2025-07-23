c6472f5205b4a6a4eb8ec9952813c5df55dbd76e
cenk <git@cenk.app>
2025-07-23 03:17:29 +0300

    0.1.61

--- src/endstone_welcome_message/welcome_message.py
+++ src/endstone_welcome_message/welcome_message.py
@@ -24,11 +24,12 @@ class WelcomeMessage(Plugin):
         if self.welcome_message_type > 0:
             self.show_message(event.player)
 
-    def show_message(self, player) -> None:
+    def show_message(self, player):
         welcome_message_header, welcome_message_body = self.replace_placeholders(player)
 
         if self.welcome_message_wait_before > 0:
             time.sleep(self.welcome_message_wait_before)
+
         match self.welcome_message_type:
             case 1:
                 player.send_message(welcome_message_body)
@@ -48,7 +49,7 @@ class WelcomeMessage(Plugin):
                 )
                 player.send_form(welcome_form)
 
-    def replace_placeholders(self, player) -> str:
+    def replace_placeholders(self, player):
         placeholder = {
             'player_name': player.name,
             'player_locale': player.locale,
@@ -56,11 +57,11 @@ class WelcomeMessage(Plugin):
             'player_device_id': player.device_id,
             'player_hostname': player.address.hostname,
             'player_port': player.address.port,
-            'player_game_mode': player.game_mode.name,
+            'player_game_mode': player.game_mode.name.capitalize(),
             'player_game_version': player.game_version,
             'player_exp_level': player.exp_level,
             'player_total_exp': player.total_exp,
-            'player_exp_progress': player.exp_progress,
+            'player_exp_progress': f"{player.exp_progress:.2f}",
             'player_ping': player.ping,
             'player_dimension_name': player.location.dimension.type.name.replace("_"," ").title(),
             'player_dimension_id': player.location.dimension.type.value,
@@ -74,7 +75,7 @@ class WelcomeMessage(Plugin):
             'server_level_name': self.server.level.name.replace("_", " ").title(),
             'server_max_players': self.server.max_players,
             'server_online_players': len(self.server.online_players),
-            'server_start_time': self.server.start_time,
+            'server_start_time': self.server.start_time.strftime('%d %b %Y %H:%M:%S'),
             'server_locale': self.server.language.locale,
             'server_endstone_version': self.server.version,
             'server_minecraft_version': self.server.minecraft_version,
